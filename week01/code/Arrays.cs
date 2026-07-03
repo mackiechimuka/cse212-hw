7045103b20bf7463797f958f67a8b7a2fd0496c8
user <mackiechimuka@gmail.com>
2026-07-04 00:09:09 +0200

    Array assignment is done

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,11 +9,20 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
-        return []; // replace this return statement with your own
+        // For this problem I need to find the multiples of a number based on the lenghth provided
+        // I will create an empty array of doubles with the size of the length provided
+        // I will then use a for loop to iterate using the lenghth as the limit 
+        // I will then multiply the number by the index of the loop and then add the result to the array
+        // Finally , I will return the array of doubles that contains the multiples of the number
+        // I realized that I need to keep track of the multiple of the number so I created another variable multiple and then incremented it by 1 after adding the multiples of the number to the array
+        double[] multiples = new double[length];
+        int multipler = 1;
+        for (int i = 0; i <= length - 1; i++)
+        {
+            multiples[i] = number * multipler;
+            multipler++;
+        }
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -26,8 +35,26 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // I will check if the data is null or the amount is less than 1 and if so I will return
+        // I will then get the length of the data 
+        //I will then get the amount modulus the length of the data to get the actual amount to rotate eg lets say its 12 and we have 9 items and we will only need 3 rotations
+        // I will check if amount is 0 and if so then I will return
+        // I will then reverse the entire list
+        // I will then reverse the first amount of items in the list
+        // I will then reverse the rest of the items in the list
+        if (data == null || amount < 1)
+        {
+            return;
+        }
+        int length = data.Count;
+        amount = amount % length;
+        if (amount == 0)
+        {
+            return;
+        }
+
+        data.Reverse();
+        data.Reverse(0,amount);
+        data.Reverse(amount,length-amount);
     }
 }
